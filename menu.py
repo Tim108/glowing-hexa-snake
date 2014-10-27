169b827c2fe507a63690fd84676f0f55d40eefba
Tim108 <tsonderen108@gmail.com>
2014-10-27 11:38:31 +0100

    main menu :D

--- menu.py
+++ menu.py
@@ -1,11 +1,3 @@
-#
-# cocos2d
-# http://cocos2d.org
-#
-# Particle Engine done by Phil Hassey
-# http://www.imitationpickles.org
-#
-
 from __future__ import division, print_function, unicode_literals
 import six
 
@@ -23,114 +15,13 @@ from cocos.layer import *
 from cocos.actions import *
 from cocos.sprite import Sprite
 
-import random
-
-rr = random.randrange
-
-
-class Fire:
-
-    def __init__(self, x, y, vy, frame, size):
-        self.x, self.y, self.vy, self.frame, self.size = x, y, vy, frame, size
-
-
-class FireManager(Layer):
-
-    def __init__(self, view_width, num):
-        super(FireManager, self).__init__()
-
-        self.view_width = view_width
-        self.goodies = []
-        self.batch = pyglet.graphics.Batch()
-        self.fimg = pyglet.resource.image('fire.jpg')
-        self.group = pyglet.sprite.SpriteGroup(self.fimg.texture,
-                                               blend_src=GL_SRC_ALPHA, blend_dest=GL_ONE)
-        self.vertex_list = self.batch.add(4 * num, GL_QUADS, self.group,
-                                          'v2i', 'c4B', ('t3f', self.fimg.texture.tex_coords * num))
-        for n in range(0, num):
-            f = Fire(0, 0, 0, 0, 0)
-            self.goodies.append(f)
-            self.vertex_list.vertices[n * 8:(n + 1) * 8] = [0, 0, 0, 0, 0, 0, 0, 0]
-            self.vertex_list.colors[n * 16:(n + 1) * 16] = [0, 0, 0, 0, ] * 4
-
-        self.schedule(self.step)
-
-    def step(self, dt):
-        w, h = self.fimg.width, self.fimg.height
-        fires = self.goodies
-        verts, clrs = self.vertex_list.vertices, self.vertex_list.colors
-        for n, f in enumerate(fires):
-            if not f.frame:
-                f.x = rr(0, self.view_width)
-                f.y = rr(-120, -80)
-                f.vy = rr(40, 70) / 100.0
-                f.frame = rr(50, 250)
-                f.size = 8 + pow(rr(0.0, 100) / 100.0, 2.0) * 32
-                f.scale = f.size / 32.0
-
-            x = f.x = f.x + rr(-50, 50) / 100.0
-            y = f.y = f.y + f.vy * 4
-            c = 3 * f.frame / 255.0
-            r, g, b = (min(255, int(c * 0xc2)), min(255, int(c * 0x41)), min(255, int(c * 0x21)))
-            f.frame -= 1
-            ww, hh = w * f.scale, h * f.scale
-            x -= ww / 2
-            if six.PY2:
-                vs = map(int, [x, y, x + ww, y, x + ww, y + hh, x, y + hh])
-            else:
-                vs = list(map(int, [x, y, x + ww, y, x + ww, y + hh, x, y + hh]))
-            verts[n * 8:(n + 1) * 8] = vs
-            clrs[n * 16:(n + 1) * 16] = [r, g, b, 255] * 4
-
-    def draw(self):
-        glPushMatrix()
-        self.transform()
-
-        self.batch.draw()
-
-        glPopMatrix()
-
-
-class SpriteLayer(Layer):
-
-    def __init__(self):
-        super(SpriteLayer, self).__init__()
-
-        sprite1 = Sprite('grossini.png')
-        sprite2 = Sprite('grossinis_sister1.png')
-        sprite3 = Sprite('grossinis_sister2.png')
-
-        sprite1.position = (320, 240)
-        sprite2.position = (620, 100)
-        sprite3.position = (20, 100)
-
-        self.add(sprite1)
-        self.add(sprite2)
-        self.add(sprite3)
-
-        ju_right = JumpBy((600, 0), height=100, jumps=4, duration=5)
-        ju_left = JumpBy((-600, 0), height=100, jumps=4, duration=5)
-        rot1 = Rotate(180 * 4, duration=5)
-
-        sprite1.opacity = 128
-
-        sc = ScaleBy(9, 5)
-        rot = Rotate(180, 5)
-
-        sprite1.do(Repeat(sc + Reverse(sc)))
-        sprite1.do(Repeat(rot + Reverse(rot)))
-        sprite2.do(Repeat(ju_left + Reverse(ju_left)))
-        sprite2.do(Repeat(Reverse(rot1) + rot1))
-        sprite3.do(Repeat(ju_right + Reverse(ju_right)))
-        sprite3.do(Repeat(rot1 + Reverse(rot1)))
-
+import random; rr = random.randrange
 
 class MainMenu(Menu):
-
-    def __init__(self):
+    def __init__( self ):
 
         # call superclass with the title
-        super(MainMenu, self).__init__("GROSSINI'S SISTERS")
+        super( MainMenu, self ).__init__("Glowing-Hexa-Snake" )
 
         pyglet.font.add_directory('.')
 
@@ -145,37 +36,37 @@ class MainMenu(Menu):
         # more info
 
         # example: menus can be vertical aligned and horizontal aligned
-        self.menu_valign = CENTER
-        self.menu_halign = CENTER
+        self.menu_valign = TOP
+        self.menu_halign = LEFT
 
         items = []
+        items.append( MenuItem('New Game', self.on_new_game ) )
+        items.append( MenuItem('Options', self.on_options ) )
+        items.append( MenuItem('Scores', self.on_scores ) )
+        items.append( MenuItem('Quit', self.on_quit ) )
 
-        items.append(MenuItem('New Game', self.on_new_game))
-        items.append(MenuItem('Options', self.on_options))
-        items.append(MenuItem('Scores', self.on_scores))
-        items.append(MenuItem('Quit', self.on_quit))
+        self.create_menu( items, zoom_in(), zoom_out() )
 
-        self.create_menu(items, zoom_in(), zoom_out())
 
     # Callbacks
-    def on_new_game(self):
-#        director.set_scene(StartGame())
+    def on_new_game( self ):
+#        director.set_scene( StartGame() )
         print("on_new_game()")
+           
 
-    def on_scores(self):
-        self.parent.switch_to(2)
+    def on_scores( self ):
+        self.parent.switch_to( 2 )
 
-    def on_options(self):
-        self.parent.switch_to(1)
+    def on_options( self ):
+        self.parent.switch_to( 1 )
 
-    def on_quit(self):
+    def on_quit( self ):
         director.pop()
 
 
 class OptionMenu(Menu):
-
-    def __init__(self):
-        super(OptionMenu, self).__init__("GROSSINI'S SISTERS")
+    def __init__( self ):
+        super( OptionMenu, self ).__init__("Glowing-Hexa-Snake" )
 
         self.font_title['font_name'] = 'You Are Loved'
         self.font_title['font_size'] = 72
@@ -183,47 +74,59 @@ class OptionMenu(Menu):
         self.font_item['font_name'] = 'You Are Loved'
         self.font_item_selected['font_name'] = 'You Are Loved'
 
-        self.menu_valign = BOTTOM
-        self.menu_halign = RIGHT
-
-        items = []
-        items.append(MenuItem('Fullscreen', self.on_fullscreen))
-        items.append(ToggleMenuItem('Show FPS: ', self.on_show_fps, True))
-        items.append(MenuItem('OK', self.on_quit))
-        self.create_menu(items, shake(), shake_back())
-
-    # Callbacks
-    def on_fullscreen(self):
-        director.window.set_fullscreen(not director.window.fullscreen)
-
-    def on_quit(self):
-        self.parent.switch_to(0)
-
-    def on_show_fps(self, value):
-        director.show_FPS = value
-
-
-class ScoreMenu(Menu):
-
-    def __init__(self):
-        super(ScoreMenu, self).__init__("GROSSINI'S SISTERS")
-
-        self.font_title['font_name'] = 'You Are Loved'
-        self.font_title['font_size'] = 72
-        self.font_item['font_name'] = 'You Are Loved'
-        self.font_item_selected['font_name'] = 'You Are Loved'
-
-        self.menu_valign = BOTTOM
+        self.menu_valign = TOP
         self.menu_halign = LEFT
 
-        self.create_menu([MenuItem('Go Back', self.on_quit)])
+        items = []
+        items.append( MenuItem('Fullscreen', self.on_fullscreen) )
+        items.append( ToggleMenuItem('Show FPS: ', self.on_show_fps, True) )
+        items.append( MenuItem('OK', self.on_quit) )
+        self.create_menu( items, zoom_in(), zoom_out() )
 
-    def on_quit(self):
-        self.parent.switch_to(0)
+    # Callbacks
+    def on_fullscreen( self ):
+        director.window.set_fullscreen( not director.window.fullscreen )
+
+    def on_quit( self ):
+        self.parent.switch_to( 0 )
+
+    def on_show_fps( self, value ):
+        director.show_FPS = value
+
+class ScoreMenu(Menu):
+    def __init__( self ):
+        super( ScoreMenu, self ).__init__("Glowing-Hexa-Snake" )
+
+        self.font_title['font_name'] = 'You Are Loved'
+        self.font_title['font_size'] = 36
+        self.font_item['font_name'] = 'You Are Loved'
+        self.font_item_selected['font_name'] = 'You Are Loved'
+
+        self.menu_valign = TOP
+        self.menu_halign = LEFT
+
+        self.create_menu( [MenuItem('Go Back', self.on_quit)] )
+
+    def on_quit( self ):
+        self.parent.switch_to( 0 )
 
 
 def init():
-    director.init(resizable=True, width=640, height=480)
+    director.init( resizable=True, width=640, height=480)
+
+def start():
+    director.set_depth_test()
+
+    menulayer = MultiplexLayer( MainMenu(), OptionMenu(), ScoreMenu() )
+    
+    scene =Scene( menulayer )
+
+    return scene
+
+def run(scene):
+    director.run( scene )
 
 if __name__ == "__main__":
     init()
+    s = start()
+    run(s)
