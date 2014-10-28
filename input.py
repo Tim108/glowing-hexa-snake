00779edd084c23e98396e241a7a1deadb0a50c9e
Tim108 <tsonderen108@gmail.com>
2014-10-28 11:16:05 +0100

    main.py

--- input.py
+++ input.py
@@ -1,1 +1,8 @@
 #catch input
+class Input.py(self):
+    def __init__(self, MainMenu):
+	print "Input initialized"
+
+    def run(self):
+	print "Input running"
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import threading
 
 class Output(threading.Thread):
+
     def __init__(self):
         execfile("output.py")
 	self.Output.__init__()
@@ -10,17 +11,28 @@ class Output(threading.Thread):
 	self.Output.run()
 
 class Menu(threading.Thread):
-     def __init__(self, Output):
+    def __init__(self, Output):
         execfile("menu.py")
 	
-     def run(self):
+    def run(self):
 	self.MainMenu.__init__()
 
+class Input(threading.Thread):
+    def __init__(self, MainMenu):
+	execfile("input.py")
+	self.Input.__init__()
+    
+    def run(self):
+	self.Input.run()
+
 outputThread = Output()   
-menuThread = Menu(outputThread)
+menuThread = Menu(outputThread.Output)
+inputThread = Input(menuThread.MainMenu)
 outputThread.start()
 menuThread.start()
+inputThread.start()
 outputThread.join()
 menuThread.join()
+inputThread.join()
 
 
