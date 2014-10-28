a7317ff14fc2796b59249b50cd9924210887baa7
Tim108 <tsonderen108@gmail.com>
2014-10-28 10:38:20 +0100

    main.. output.. stuff

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 #Main
+import threading
+
+class Output(threading.Thread):
+    def __init__(self):
+        execfile("output.py")
+	self.Output.__init__()
+
+    def run(self):
+	self.Output.run()
+
+class Menu(threading.Thread):
+     def __init__(self, Output):
+        execfile("menu.py")
+	
+     def run(self):
+	self.MainMenu.__init__()
+
+outputThread = Output()   
+menuThread = Menu(outputThread)
+outputThread.start()
+menuThread.start()
+outputThread.join()
+menuThread.join()
+
+
