20efdd8362eaa12f79b164e637c67ae66d504d17
Tim108 <tsonderen108@gmail.com>
2014-10-29 16:10:04 +0100

    wooooo ik kan weer zien wat ik type na uitvoeren van maingit add --all :DDDDD

--- output.py
+++ output.py
@@ -7,16 +7,6 @@ import atexit
 class Output(threading.Thread):
     def __init__(self, lock):
 	self.lock = lock
-	print "herman"
-	fd = sys.stdin.fileno()
-    
-        oldterm = termios.tcgetattr(fd)
-        newattr = termios.tcgetattr(fd)
-        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
-        termios.tcsetattr(fd, termios.TCSANOW, newattr)
-    
-        oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
-        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
  
         GPIO.setmode(GPIO.BCM)
 
