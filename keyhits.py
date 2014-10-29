4e4078189c7ebb4dd8d995358e987b5843201b18
Tim108 <tsonderen108@gmail.com>
2014-10-29 16:50:13 +0100

    keyboard input werkt :D

--- keyhits.py
+++ keyhits.py
@@ -1,12 +1,48 @@
 #guiInput
-import sys
+import threading
+import termios, fcntl, sys, os
 
-class guiIn(self, threading.Thread):
+class guiIn():
     gui = 0
-    def __init__(self, menu):
-	gui = menu
+    def __init__(self):
+	#gui = menu
+	self.run()
 
     def run(self):
-	while(True):
-	    char = sys.stdin.read(1)
-	    print 'You pressed %s' % char
+	fd = sys.stdin.fileno()
+
+	oldterm = termios.tcgetattr(fd)
+	newattr = termios.tcgetattr(fd)
+	newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+	termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+	oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+	fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+	try:
+		print "herman"
+		while(True):
+		    try:
+		        char = sys.stdin.read(1)
+		        self.processIn(char)
+		    except IOError: pass
+		    except KeyboardInterrupt: pass
+        except KeyboardInterrupt: pass
+	finally:
+	    termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+	    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+
+    def processIn(self, char):
+	if(char == 'w'):
+		print "Go up"
+	elif(char == 'a'):
+		print "Go left"
+	elif(char == 's'):
+		print "Go down"
+	elif(char == 'd'):
+		print "Go right"
+	elif(char == 'p'):
+		print "Pause"
+	elif(char == 'r'):
+		print "Reset"
+    
+guiIn()
