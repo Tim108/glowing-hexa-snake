7d460504588c9f6cfd088c56c0eab5ce3400101d
Tim108 <tsonderen108@gmail.com>
2014-10-30 12:06:59 +0100

    getest met fpga, output werkt :D

--- output.py
+++ output.py
@@ -10,74 +10,84 @@ class Output(threading.Thread):
 	GPIO.setwarnings(False) 
         GPIO.setmode(GPIO.BCM)
 
-        GPIO.setup(8, GPIO.OUT)
-        GPIO.setup(10, GPIO.OUT)
+        GPIO.setup(14, GPIO.OUT)
+        GPIO.setup(15, GPIO.OUT)
+        GPIO.setup(2, GPIO.OUT)
         GPIO.setup(3, GPIO.OUT)
-        GPIO.setup(5, GPIO.OUT)
+
+	self.reset()
 
     def reset(self):
-	GPIO.output(8, GPIO.LOW)
-	GPIO.output(10, GPIO.LOW)
+	GPIO.output(14, GPIO.LOW)
+	GPIO.output(15, GPIO.LOW)
+	GPIO.output(2, GPIO.LOW)
 	GPIO.output(3, GPIO.LOW)
-	GPIO.output(5, GPIO.LOW)
 
     def up(self):
-	self.lock.acquire()
-	self.reset()
-	GPIO.output(5, GPIO.HIGH)
-	self.lock.release()
-
-    def down(self):
-	self.lock.acquire()
-	self.reset()
-	GPIO.output(8, GPIO.HIGH)
-	GPIO.output(5, GPIO.HIGH)
-	self.lock.release()
-
-    def left(self):
-	self.lock.acquire()
-	self.reset()
-	GPIO.output(10, GPIO.HIGH)
-	GPIO.output(5, GPIO.HIGH)
-	self.lock.release()
-
-    def right(self):
-	self.lock.acquire()
-	self.reset()
-	GPIO.output(8, GPIO.HIGH)
-	GPIO.output(10, GPIO.HIGH)
-	GPIO.output(5, GPIO.HIGH)
-	self.lock.release()
-
-    def pause(self):
+	print "setting pins for up"
 	self.lock.acquire()
 	self.reset()
 	GPIO.output(3, GPIO.HIGH)
-	GPIO.output(5, GPIO.HIGH)
+	self.lock.release()
+	print "pins set for up"
+
+    def down(self):
+	print "setting pins for down"
+	self.lock.acquire()
+	self.reset()
+	GPIO.output(14, GPIO.HIGH)
+	GPIO.output(3, GPIO.HIGH)
+	self.lock.release()
+	print "pins set for down"
+
+    def left(self):
+	print "setting pins for left"
+	self.lock.acquire()
+	self.reset()
+	GPIO.output(15, GPIO.HIGH)
+	GPIO.output(3, GPIO.HIGH)
+	self.lock.release()
+	print "pins set for left"
+
+    def right(self):
+	print "setting pins for right"
+	self.lock.acquire()
+	self.reset()
+	GPIO.output(14, GPIO.HIGH)
+	GPIO.output(15, GPIO.HIGH)
+	GPIO.output(3, GPIO.HIGH)
+	self.lock.release()
+	print "pins set for right"
+
+    def bPause(self):
+	self.lock.acquire()
+	self.reset()
+	GPIO.output(2, GPIO.HIGH)
+	GPIO.output(3, GPIO.HIGH)
 	self.lock.release()
 
     def bReset(self):
 	self.lock.acquire()
 	self.reset()
-	GPIO.output(8, GPIO.HIGH)
+	GPIO.output(14, GPIO.HIGH)
+	GPIO.output(2, GPIO.HIGH)
 	GPIO.output(3, GPIO.HIGH)
-	GPIO.output(5, GPIO.HIGH)
 	self.lock.release()
 
     def bStart(self):
 	self.lock.acquire()
 	self.reset()
-	GPIO.output(10, GPIO.HIGH)
+	GPIO.output(15, GPIO.HIGH)
+	GPIO.output(2, GPIO.HIGH)
 	GPIO.output(3, GPIO.HIGH)
-	GPIO.output(5, GPIO.HIGH)
 	self.lock.release()
 
     def bExit(self):
 	self.lock.acquire()
 	self.reset()
-	GPIO.output(8, GPIO.HIGH)
-	GPIO.output(10, GPIO.HIGH)
+	GPIO.output(14, GPIO.HIGH)
+	GPIO.output(15, GPIO.HIGH)
+	GPIO.output(2, GPIO.HIGH)
 	GPIO.output(3, GPIO.HIGH)
-	GPIO.output(5, GPIO.HIGH)
 	self.lock.release()
 
